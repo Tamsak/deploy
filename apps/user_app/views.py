dc6e35899032282d10adb1ad575b8c69c281f224
Nadia <ntisthebest@gmail.com>
2018-04-29 03:19:07 -0700

    initial commit

--- apps/user_app/views.py
+++ apps/user_app/views.py
@@ -36,3 +36,5 @@ def create(request):
         User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'],email=request.POST['email'])
     return redirect('/users')
 
+
+
